06357527ef83cf5d0ca7d39d9eca896423356283
Jimmys Mac <eadesj25@gmail.com>
2023-03-10 07:47:02 -0500

    added confirm messagess for letters, numbers, & symbols

--- script.js
+++ script.js
@@ -10,6 +10,15 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var length = prompt("how long would you like your password to be?")
+  var hasUpperCase = confirm("would you like Upper case letters in your password?")
+  var hasLowerCase = confirm("would you like to use lower case letters?")
+  var hasNumbers = confirm("would you like to use numbers?")
+  var hasSymbols = confirm("would you like to use symbols?")
+
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
